241bdb74535ccfc17f0dc4f2e97f29ac9103f8be
Aissa Laribi <57592948+aissa-laribi@users.noreply.github.com>
2022-02-06 08:55:45 +0000

    Update new-user-least-privilege.sh

--- IAM/new-user-least-privilege.sh
+++ IAM/new-user-least-privilege.sh
@@ -1,6 +1,6 @@
 aws iam create-user --user <username>
-aws iam create-access-key --user-name <username>
+aws iam create-access-key --user-name <username> > <username>_credentials.txt
 aws iam list-policies | grep <service-keyword>
 aws iam attach-user-policy --user-name  --policy-arn <copy and paste the arn from the output of the last outcome>
-##If the User's Access keys are lost type the following command:
+##Check if the User's Access keys has been created:
 aws iam list-access-keys --user-name <user-name>
